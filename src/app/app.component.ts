ee3a123bd54d0f9f61d29dde88bd5bee48c830af
AlexeyPupykin <mymailvrn98@gmail.com>
2023-10-09 14:26:13 +0300

    implement task;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  Observable,
+  Subject,
+  delay,
+  map,
+  merge,
+  share,
+  switchMap,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MegaFonTestTask';
+  first$: Observable<number> = new Observable<number>();
+  second$: Observable<number> = new Observable<number>();
+  third$: Observable<number> = new Observable<number>();
+
+  update$: Subject<void> = new Subject<void>();
+
+  constructor(private readonly _http: HttpClient) {
+    const data = merge(
+      this.update$.pipe(
+        switchMap(() => {
+          return this._getData();
+        }),
+        share()
+      )
+    );
+
+    this.first$ = data.pipe(delay(500));
+    this.second$ = data.pipe(delay(1000));
+    this.third$ = data.pipe(delay(1500));
+  }
+
+  update() {
+    this.update$.next();
+  }
+
+  private _getData(): Observable<number> {
+    return this._http
+      .get('https://api.rand.by/v1/integer?count=1')
+      .pipe(map((value: any) => value.items[0]));
+  }
 }
